06aa3d5205ac40415240c29f4c56bf5816800920
Shreya Sarker <shreya.sarker@gmail.com>
2026-02-12 00:11:04 +0600

    Add files via upload

---
+++ Lesson 1/activity.py
@@ -0,0 +1,6 @@
+print("Welcome to the Python Class")
+# Printing about hobby
+print("My hobby is Coding.")
+# Printing what is 5 + 3
+print("The sum of 5 and 3 is", 5 + 3)
+print("The sum of 5 and 3 is", 8)
